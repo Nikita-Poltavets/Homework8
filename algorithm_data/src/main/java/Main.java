d5f3f8340e13fa99bc440155c723ca573a72f91f
Nikita-Poltavets <mykyta.poltavets@nure.ua>
2020-07-05 19:25:58 +0300

    Create bubble sort algorithm

--- algorithm_data/src/main/java/Main.java
+++ algorithm_data/src/main/java/Main.java
@@ -1,15 +1,22 @@
+import util.BubbleSort;
+
 public class Main {
 
-    public void getArray(){
+    public static int[] getArray(){
         int[] ints = new int[100000];
         int index = 0;
         for (int i = 0 ; i < ints.length ; i++) {
             index = (int) Math.floor(Math.random() * 100000);
             ints[i] = index;
         }
-        for (int anInt : ints) {
-            System.out.println("anInt = " + anInt);
-        }
+
+        return ints;
+    }
+
+    public static void main(String[] args) {
+
+        BubbleSort.bubbleSortAlgorithm(getArray());
+
     }
 
 }
