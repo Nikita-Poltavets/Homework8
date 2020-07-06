44144d5f8028c509f85eadb3a45023e698efdff5
Nikita-Poltavets <mykyta.poltavets@nure.ua>
2020-07-06 18:29:42 +0300

    Add functionality for measuring the runtime of sorting for each of the algorithms

--- algorithm_main/src/main/java/com/homework/nix/main/Main.java
+++ algorithm_main/src/main/java/com/homework/nix/main/Main.java
@@ -2,10 +2,12 @@ package com.homework.nix.main;
 
 import com.homework.nix.library.util.*;
 
+import java.util.*;
+
 public class Main {
 
     public static int[] getArray(){
-        int[] ints = new int[100000];
+        int[] ints = new int[10000];
         int index = 0;
         for (int i = 0 ; i < ints.length ; i++) {
             index = (int) Math.floor(Math.random() * 100000);
@@ -15,21 +17,49 @@ public class Main {
         return ints;
     }
 
-    public static void main(String[] args) {
+    public static void algrorithms(){
+        long startTime;
+       Map<Long, String> map = new HashMap<>();
 
+
+        startTime = System.nanoTime();
         BubbleSort.bubbleSortAlgorithm(getArray());
+        map.put(System.nanoTime() - startTime, "Bubble sort");
 
+        startTime = System.nanoTime();
         InsertionSort.insertionSortAlgorithm(getArray());
+        map.put(System.nanoTime() - startTime, "Insertion sort");
 
-        MergeSort.mergeArray(getArray(), getArray());
+        startTime = System.nanoTime();
+        MergeSort.mergeArray(Arrays.copyOfRange(getArray(), 0, getArray().length / 2), Arrays.copyOfRange(getArray(), getArray().length / 2, getArray().length));
+        map.put(System.nanoTime() - startTime, "Merge sort");
 
+        startTime = System.nanoTime();
         QuickSort.quickSortAlgorithm(getArray(), 0, getArray().length - 1);
+        map.put(System.nanoTime() - startTime, "Quick sort");
 
+        startTime = System.nanoTime();
         SelectionSort.sort(getArray());
+        map.put(System.nanoTime() - startTime, "Selection sort");
 
+        startTime = System.nanoTime();
         ShellSort.sort(getArray());
+        map.put(System.nanoTime() - startTime, "Shell sort");
 
+        startTime = System.nanoTime();
         ShuttleSort.shuttleSort(getArray());
+        map.put(System.nanoTime() - startTime, "Shuttle sort");
+
+        Map<Long, String> sortedMap = new TreeMap<>(map);
+
+        for(Map.Entry<Long, String> pair : sortedMap.entrySet())
+        {
+            System.out.println(pair.getKey() + " - " + pair.getValue());
+        }
+    }
+
+    public static void main(String[] args) {
+        algrorithms();
     }
 
 }
