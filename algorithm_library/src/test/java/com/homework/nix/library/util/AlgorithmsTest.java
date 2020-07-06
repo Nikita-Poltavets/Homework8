e752d8802a16e76bcb2ea3e904133073d64b837e
Nikita-Poltavets <mykyta.poltavets@nure.ua>
2020-07-06 19:01:08 +0300

    Create test for bubble sort algorithm

---
+++ algorithm_library/src/test/java/com/homework/nix/library/util/AlgorithmsTest.java
@@ -0,0 +1,17 @@
+package com.homework.nix.library.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AlgorithmsTest {
+
+    int[] sortedArray = {41, 42, 43, 44, 45, 46, 47, 48};
+    int[] unsortedArray = {42, 44, 41, 48, 45, 43, 46, 47};
+
+    @Test
+    void bubbleSortAlgorithm() {
+        BubbleSort.bubbleSortAlgorithm(unsortedArray);
+        assertArrayEquals(sortedArray, unsortedArray);
+    }
+}
